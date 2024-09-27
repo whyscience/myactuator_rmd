c87fc5bbdca2716baa349c86e78cf39c1b5b964f
eric <haiyang.wang@clobotics.com>
2024-09-27 10:44:01 +0800

    debug

--- src/esp32_can.py
+++ src/esp32_can.py
@@ -1,5 +1,8 @@
-import can
+import threading
 import time
+
+import can
+
 
 # Setup the CAN interface
 def setup_can_interface(interface='can0', bitrate=1000000):
@@ -11,14 +14,15 @@ def setup_can_interface(interface='can0', bitrate=1000000):
         print(f"Error setting up CAN interface: {e}")
         exit(1)
 
+
 # Function to receive and process CAN message
 def receive_can_messages(bus):
     idx = 0
     while True:
         message = bus.recv(1.0)  # Timeout of 1 second
         if message is not None:
-            print(f"Received packet with id 0x{message.arbitration_id:X}")
-
+            print(f"Received packet with id 0x{message.arbitration_id:X}, idx={idx}")
+            idx += 1
             # Check if it's an extended frame
             if message.is_extended_id:
                 print("Extended frame")
@@ -27,27 +31,38 @@ def receive_can_messages(bus):
             if message.is_remote_frame:
                 print("RTR frame and requested length", message.dlc)
             else:
-                print(f"Data length: {message.dlc}")
+                # print(f"Data length: {message.dlc}")
                 print("Data: ", end="")
                 for byte in message.data:
-                    print(chr(byte), end="")
+                    # print data in Hex
+                    print(f"{byte:X} ", end="")
                 print()
         else:
-            print("No message received {}".format(idx))
-            idx += 1
+            # if idx % 10 == 0:
+            #     print("No message received {}".format(idx))
+            pass
+
+
 
 def send_can_messages(bus):
-    message = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], is_extended_id=False)
-    try:
-        bus.send(message)
-        print("Message sent on {}".format(bus.channel_info))
-    except can.CanError:
-        print("Message NOT sent")
-    time.sleep(1)
+    message = can.Message(arbitration_id=0x141, data=[0, 1, 2, 3, 4, 5, 6, 7], is_extended_id=False)
+    count = 0
+    while True:
+        try:
+            bus.send(message)
+            print("{} Message sent on {}".format(count, bus.channel_info))
+            count += 1
+        except can.CanError:
+            print("Message NOT sent")
+        time.sleep(5)
+
 
 # Main function to initialize and receive messages
 def main():
     bus = setup_can_interface()
+    # add a thread to send messages
+    send_thread = threading.Thread(target=send_can_messages, args=(bus,))
+    send_thread.start()
 
     try:
         while True:
@@ -55,6 +70,7 @@ def main():
             # send_can_messages(bus)
     except KeyboardInterrupt:
         print("\nCAN Receiver stopped")
+
 
 if __name__ == "__main__":
     main()
